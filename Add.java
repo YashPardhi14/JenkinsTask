3e6cac51e5368dcdd4c1999e703813881cf7058b
YashPardhi14 <yapardhi@gmail.com>
2022-10-21 01:08:09 +0530

    updated

--- Add.java
+++ Add.java
@@ -3,7 +3,7 @@ public class Add {
 	public static void main(String[] args) {
 		System.out.println("Sum of two odd Number Is always EVEN");
 		System.out.println("Example:" + "5" + "+" + "7" + "=" + (5 + 7));
-
+            System.out.println("Thank you!"); 
 	}
 
 }
