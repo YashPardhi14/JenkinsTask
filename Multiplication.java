443a6f3de2582b34254cdc88643da5beea250f45
YashPardhi14 <yapardhi@gmail.com>
2022-10-21 00:56:01 +0530

    done

--- Multiplication.java
+++ Multiplication.java
@@ -2,6 +2,7 @@ public class Multiplication {
 
 	public static void main(String[] args) {
 		 System.out.println("Multiplication of 3 and 8 is=" +(3*8));
+              System.out.println("Multiplication of 5 and 7 is=" +(5*7));
 
 	}
 
