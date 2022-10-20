70e40488338e99f84bc8cb613d0a8ba661aacfea
YashPardhi14 <yapardhi@gmail.com>
2022-10-21 00:14:31 +0530

    in branch b1

---
+++ Add.java
@@ -0,0 +1,9 @@
+public class Add {
+
+	public static void main(String[] args) {
+		System.out.println("Sum of two odd Number Is always EVEN");
+		System.out.println("Example:" + "5" + "+" + "7" + "=" + (5 + 7));
+
+	}
+
+}
